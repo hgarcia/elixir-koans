116f1f9557e87de4d80630290345cc67c31571c0
skovsgaard <bastian.1343@gmail.com>
2014-09-17 23:18:39 +0200

    Added function and fixed existing.

--- test/02_functions_test.exs
+++ test/02_functions_test.exs
@@ -3,19 +3,19 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule FunctionsTest do
   use ExUnit.Case
 
-  test "modify the left hand side to pass the expression" do
+  test "modify the right hand side to pass the assertion" do
     sum = fn (a,b) -> a + b end
     assert sum.(5,5) == 0
   end
 
-  test "modify the anonymous function body to pass the expression" do
+  test "modify the anonymous function body to pass the assertion" do
     multi = fn(a,b) -> 0 end
     assert multi.(5,6) == 30
   end
 
-  test "modify the anonymous function to pass the expression" do
-    sub = fn(a) -> a - b end
-    assert fn(1,2) == 1
+  test "modify the anonymous function to pass the assertion" do
+    sub = fn(a) -> a end
+    assert sub.(1,2) == 1
   end
 
   def new_name(name), do: "" 
@@ -23,8 +23,16 @@ defmodule FunctionsTest do
     assert new_name("José") == "Chris"
   end
 
-  def put_in_list(name), do name
+  def put_in_list(name), do: name
   test "modify the named function above to pass the assertion" do
     assert put_in_list("Bruce") == ["Bruce"]
+  end
+
+  def get_first_in_list(list) do
+    [head|tail] = list
+    head
+  end
+  test "modify the right hand side to pass the assertion" do
+    assert get_first_in_list([1,2,3]) == 0
   end
 end
