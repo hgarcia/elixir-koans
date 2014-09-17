114ddffad3a6a6ba4e0d594660b9b116ba5e4eaa
skovsgaard <bastian.1343@gmail.com>
2014-09-17 19:50:17 +0200

    Expanded 02_functions_test.exs

--- test/02_functions_test.exs
+++ test/02_functions_test.exs
@@ -3,10 +3,28 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule FunctionsTest do
   use ExUnit.Case
 
-  test "modify the right hand to pass the expression" do
-    a = []
-    assert [1, 2, 3] = a
+  test "modify the left hand side to pass the expression" do
+    sum = fn (a,b) -> a + b end
+    assert sum.(5,5) == 0
   end
 
+  test "modify the anonymous function body to pass the expression" do
+    multi = fn(a,b) -> 0 end
+    assert multi.(5,6) == 30
+  end
 
+  test "modify the anonymous function to pass the expression" do
+    sub = fn(a) -> a - b end
+    assert fn(1,2) == 1
+  end
+
+  def new_name(name), do: "" 
+  test "modify the named function above to pass the assertion" do
+    assert new_name("José") == "Chris"
+  end
+
+  def put_in_list(name), do name
+  test "modify the named function above to pass the assertion" do
+    assert put_in_list("Bruce") == ["Bruce"]
+  end
 end
